7623bf1de6d75d98f4c1908eeb61142b5520ba2c
Daniel Fonseca <daniel.fonseca@ccc.ufcg.edu.br>
2025-02-23 16:01:14 -0300

    .env adicionado. database.js alterado para refletir o que está no docker.

--- src/database.js
+++ src/database.js
@@ -4,8 +4,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'GP_BD',
-  password: process.env.DB_PASS || 'postgres',
+  database: process.env.DB_NAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
   port: process.env.DB_PORT || 5432,
 });
 
